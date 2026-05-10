5ff31bfaa79de9b5c537050e0202a98f0279121c
qinfei12 <166202156+qinfei12@users.noreply.github.com>
2026-05-10 21:25:16 +0800

    Fix indentation in anon_file.c

--- src/util/anon_file.c
+++ src/util/anon_file.c
@@ -119,10 +119,10 @@ get_or_create_user_temp_dir(void) {
 
     env = os_get_option("XDG_RUNTIME_DIR");
     if (env && env[0] != '\0') {
-        n = asprintf(&buf, "%s", env);
-        if (n < 0)
-           return NULL;
-        return buf;
+    n = asprintf(&buf, "%s", env);
+    if (n < 0)
+       return NULL;
+    return buf;
     }
 
     n = asprintf(&buf, "/tmp/xdg-runtime-mesa-%ld", (long)getuid());
